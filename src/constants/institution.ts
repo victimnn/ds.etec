a883dbcbb3cbef2dc5d3672b3491db92ba32a0ed
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2026-02-15 14:48:25 -0300

    feat: Criando constantes

---
+++ src/constants/institution.ts
@@ -0,0 +1,58 @@
+export const institutionContact = {
+  name: 'Etec Joao Belarmino',
+  unit: 'Centro Paula Souza',
+  serviceHoursText: 'Segunda a Sexta: 7h as 23h',
+  phoneContacts: [
+    {
+      title: 'Secretaria',
+      description: 'Atendimento geral',
+      display: '(19) 3808-1016',
+      href: 'tel:+551938081016',
+    },
+    {
+      title: 'Coordenacao',
+      description: 'Informacoes gerais',
+      display: '(19) 3807-2288',
+      href: 'tel:+551938072288',
+    },
+    {
+      title: 'Suporte',
+      description: 'Assistencia tecnica',
+      display: '(19) 3807-8982',
+      href: 'tel:+551938078982',
+    },
+  ],
+  emailContacts: [
+    {
+      title: 'Academico',
+      description: 'Para questoes relacionadas ao curso e atividades academicas',
+      address: 'e067acad@cps.sp.gov.br',
+    },
+    {
+      title: 'Administrativo',
+      description: 'Para questoes administrativas e burocraticas',
+      address: 'e067adm@cps.sp.gov.br',
+    },
+    {
+      title: 'APM',
+      description: 'Para questoes relacionadas a Associacao de Pais e Mestres',
+      address: 'apmjb@yahoo.com.br',
+    },
+  ],
+  address: {
+    street: 'Rua Sete de Setembro, 299',
+    district: 'Centro',
+    cityState: 'Amparo - SP',
+    zip: '13900-372',
+    mapsQueryUrl:
+      'https://maps.google.com/?q=Rua+Sete+de+Setembro,+299+-+Centro,+Amparo+-+SP,+13900-372',
+    mapsEmbedUrl:
+      'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14722.41243506315!2d-46.76570223809814!3d-22.705814173334975!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c92147d6b7f687%3A0x47221efa058bc901!2sEscola%20T%C3%A9cnica%20Estadual%20Jo%C3%A3o%20Belarmino!5e0!3m2!1spt-BR!2sus!4v1756689672853!5m2!1spt-BR!2sus',
+  },
+  socialLinks: {
+    instagramCourse: 'https://instagram.com/ds.etec',
+    instagramSchool: 'https://instagram.com/etecjboficial',
+    youtubeSchool: 'https://www.youtube.com/c/ETECJo%C3%A3oBelarminoOFICIAL',
+  },
+}
+
---
+++ src/constants/navigation.ts
@@ -0,0 +1,24 @@
+import { MAIN_SITE_URL } from '@/src/lib/site-urls'
+import { institutionContact } from '@/src/constants/institution'
+import { NavigationItem } from '@/src/types'
+
+export const navigationItems: NavigationItem[] = [
+  { name: 'Inicio', href: '/' },
+  { name: 'Sobre o Curso', href: '/sobre' },
+  { name: 'Projetos', href: '/projetos' },
+  { name: 'Vestibulinho', href: '/vestibulinho' },
+  { name: 'Mercado de Trabalho', href: '/mercado' },
+  { name: 'Contato', href: '/contato' },
+]
+
+export const siteConfig = {
+  name: 'Desenvolvimento de Sistemas',
+  institution: 'Etec Joao Belarmino',
+  description:
+    'Curso Tecnico em Desenvolvimento de Sistemas integrado ao Ensino Medio',
+  url: MAIN_SITE_URL,
+  ogImage: '/logo-ds.png',
+  links: {
+    instagram: institutionContact.socialLinks.instagramCourse,
+  },
+}
